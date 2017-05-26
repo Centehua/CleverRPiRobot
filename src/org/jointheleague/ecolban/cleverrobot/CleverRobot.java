7c524eba2af8d83c91b821dc72e06f9f80379e09
YASHWINLM <league@WTS10.attlocal.net>
2017-05-26 16:57:00 -0700

    RPI 3

--- src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
+++ src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
@@ -29,6 +29,10 @@ public class CleverRobot extends IRobotAdapter {
 
 	private void setup() throws Exception {
 		driveDirect(100,100);
+		Thread.sleep(1000);
+		driveDirect(50,100);
+		Thread.sleep(50000); 
+		
 	}
 	
 	private boolean loop() throws Exception{
